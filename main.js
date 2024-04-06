69f654cad63a705693a2f26d85983cac07c3b1dc
D@reK@r@n18 <xpresskaran98@gmail.com>
2024-04-06 14:34:30 +0530

    implemented the basic web page loading in electon js

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log("Hello World!");
+import { app, BrowserWindow } from "electron";
+
+const createWindow = (windowWidth, windowHeight, filePath) => {
+  const window = new BrowserWindow({
+    minWidth: windowWidth,
+    minHeight: windowHeight,
+    resizable: true,
+  });
+
+  window.loadFile(filePath);
+};
+
+app.whenReady().then(() => {
+  createWindow(1300, 800, "index.html");
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  // quit if OS is not Mac-Os
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
